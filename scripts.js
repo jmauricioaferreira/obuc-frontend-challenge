19286c81c15cf05b8af1a0195a3415101c2c7333
Jose Mauricio <jmauricio.a.ferreira@gmail.com>
2021-02-13 15:25:52 -0300

    validating form values

--- scripts.js
+++ scripts.js
@@ -1,9 +1,3 @@
-const Form = {
-  submit(event) {
-    event.preventDefault();
-  },
-};
-
 const EmployeeInfo = {
   all: [
     {
@@ -23,34 +17,34 @@ const EmployeeInfo = {
     },
   ],
 
-  add(employeeWorkplace) {
-    EmployeeInfo.all.push(employeeWorkplace);
+  add(employeeInfo) {
+    EmployeeInfo.all.push(employeeInfo);
   },
 
   remove(index) {
     EmployeeInfo.all.splice(index, 1);
-
     App.reload();
   },
 };
 
 const DOM = {
   workplaceContainer: document.querySelector("#data-table tbody"),
-  addTableRow(employeesWorkplace, index) {
-    console.log(employeesWorkplace);
+
+  addTableRow(employeeInfo, index) {
     const tr = document.createElement("tr");
-    tr.innerHTML = DOM.innerHTMLInformation(employeesWorkplace);
+    tr.innerHTML = DOM.innerHTMLInformation(employeeInfo, index);
+    tr.dataset.index = index;
     DOM.workplaceContainer.appendChild(tr);
   },
 
-  innerHTMLInformation(employeesWorkplace) {
+  innerHTMLInformation(employeeInfo, index) {
     const html = ` 
-      <td>${employeesWorkplace.employee}</td>
-      <td>${employeesWorkplace.building}</td>
-      <td>${employeesWorkplace.workplace}</td>
+      <td>${employeeInfo.employee}</td>
+      <td>${employeeInfo.building}</td>
+      <td>${employeeInfo.workplace}</td>
       <td>
         <i class="fas fa-pencil-alt"></i>
-        <i class="fas fa-trash"></i>
+        <i class="fas fa-trash" onclick="EmployeeInfo.remove(${index})"></i>
       </td>
     `;
     return html;
@@ -61,10 +55,54 @@ const DOM = {
   },
 };
 
+const Form = {
+  employee: document.querySelector("input#employee"),
+  building: document.querySelector("select#building"),
+  workplace: document.querySelector("input#workplace"),
+
+  getValues() {
+    return {
+      employee: Form.employee.value,
+      building: Form.building.value,
+      workplace: Form.workplace.value,
+    };
+  },
+
+  validateFields() {
+    const { employee, building, workplace } = Form.getValues();
+
+    if (
+      employee.trim() === "" ||
+      building.trim() === "" ||
+      workplace.trim() === ""
+    ) {
+      throw new Error("Por favor, preencha todos os campos");
+    }
+  },
+
+  clearFields() {
+    Form.employee.value = "";
+    Form.building.value = "";
+    Form.workplace.value = "";
+  },
+
+  submit(event) {
+    event.preventDefault();
+    try {
+      Form.validateFields();
+      const employeeInfo = Form.getValues();
+      DOM.addTableRow(employeeInfo);
+      Form.clearFields();
+    } catch (error) {
+      alert(error.message);
+    }
+  },
+};
+
 const App = {
   init() {
-    EmployeeInfo.all.forEach((employeeWorkplace) => {
-      DOM.addTableRow(employeeWorkplace);
+    EmployeeInfo.all.forEach((employeeInfo, index) => {
+      DOM.addTableRow(employeeInfo, index);
     });
   },
 
@@ -75,5 +113,3 @@ const App = {
 };
 
 App.init();
-
-EmployeeInfo.remove(0);
