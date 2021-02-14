d13de8cdb681f6dda32c098a4a4984caed9e647d
Jose Mauricio <jmauricio.a.ferreira@gmail.com>
2021-02-14 13:11:12 -0300

    making the table editable

--- scripts.js
+++ scripts.js
@@ -19,6 +19,53 @@ const EmployeeInfo = {
     EmployeeInfo.all.splice(index, 1);
     App.reload();
   },
+
+  edit(event) {
+    let employee = event.target.parentNode.parentNode.children[0];
+    let building = event.target.parentNode.parentNode.children[1];
+    let workplace = event.target.parentNode.parentNode.children[2];
+    let icons = event.target.parentNode.parentNode.children[3];
+
+    employee.innerHTML = `
+      <input type="text" value="${employee.innerHTML}">
+    `;
+
+    building.innerHTML = `
+    <select id="select-edit" name="building value="">
+      <option value="Prédio 1">Prédio 1</option>
+      <option value="Prédio 2">Prédio 2</option>
+      <option value="Prédio 3">Prédio 3</option>
+    </select>
+    `;
+
+    workplace.innerHTML = `
+    <input type="text" value="${workplace.innerHTML}">
+    `;
+
+    icons.innerHTML = `
+    <i class="fas fa-check" onclick="EmployeeInfo.save(event)"></i>
+    <i class="fas fa-times" onclick="EmployeeInfo.cancel(event)"></i>`;
+  },
+
+  save(event, index) {
+    let employee = event.target.parentNode.parentNode.children[0];
+    let building = event.target.parentNode.parentNode.children[1];
+    let workplace = event.target.parentNode.parentNode.children[2];
+    let icons = event.target.parentNode.parentNode.children[3];
+
+    employee.innerHTML = employee.children[0].value;
+    building.innerHTML = building.children[0].value;
+    workplace.innerHTML = workplace.children[0].value;
+
+    icons.innerHTML = `
+    <i class="fas fa-pencil-alt" onclick="EmployeeInfo.edit(event)" id="edit"></i>
+    <i class="fas fa-trash" onclick="EmployeeInfo.remove(${index})"></i>
+    `;
+  },
+
+  cancel(event) {
+    App.reload();
+  },
 };
 
 const DOM = {
@@ -33,11 +80,11 @@ const DOM = {
 
   innerHTMLInformation(employeeInfo, index) {
     const html = ` 
-      <td>${employeeInfo.employee}</td>
-      <td>${employeeInfo.building}</td>
-      <td>${employeeInfo.workplace}</td>
+      <td id="employee-value">${employeeInfo.employee}</td>
+      <td id="building-value">${employeeInfo.building}</td>
+      <td id="workplace-value">${employeeInfo.workplace}</td>
       <td>
-        <i class="fas fa-pencil-alt"></i>
+        <i class="fas fa-pencil-alt" onclick="EmployeeInfo.edit(event)" id="edit"></i>
         <i class="fas fa-trash" onclick="EmployeeInfo.remove(${index})"></i>
       </td>
     `;
